ada0fba132a95458b8f0c6a7a701c57f1cb67f94
admazzaglia <admazzaglia@gmail.com>
2023-01-19 17:27:25 -0300

    c2 deploy firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,9 @@
-export const environment = {
-  firebase: {
-    projectId: 'madfrontend-d13f8',
-    appId: '1:346558459143:web:8a9fa933681c4cf09c69a2',
-    storageBucket: 'madfrontend-d13f8.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyABBYbDL0VWpS-jKpjFQRHOyantgG_MRl4',
-    authDomain: 'madfrontend-d13f8.firebaseapp.com',
-    messagingSenderId: '346558459143',
-  },
 
-  production: true,
-    URL: 'http://localhost:8080/'
+
+export const environment = {
+  production: false,
+   URL: 'http://localhost:8080/'
 
 };
+
 
